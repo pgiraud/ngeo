b92436fac3a09528bf331b50507d6256fcabbc3b
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2016-01-22 16:36:27 +0100

    Update GitHub pages

--- app_desktop_/examples/contribs/gmf/apps/desktop/js/desktop.js
+++ app_desktop_/examples/contribs/gmf/apps/desktop/js/desktop.js
@@ -1,8 +1,9 @@
 /**
  * @fileoverview Application entry point.
  *
- * This file defines the "app_desktop" Closure namespace, which is be used as the
- * Closure entry point (see "closure_entry_point" in the "build.json" file).
+ * This file defines the "app_desktop" Closure namespace, which is be used as
+ * the Closure entry point (see "closure_entry_point" in the "build.json"
+ * file).
  *
  * This file includes `goog.require`'s for all the components/directives used
  * by the HTML page and the controller to provide the configuration.
