3aae59cf15add690d772f1e88aba6684f829de16
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2016-02-25 15:10:48 +0100

    Print layout prototype

--- contribs/gmf/src/controllers/abstractdesktop.js
+++ contribs/gmf/src/controllers/abstractdesktop.js
@@ -7,6 +7,10 @@ goog.require('gmf.mobileBackgroundlayerselectorDirective');
 /** @suppress {extraRequire} */
 goog.require('ngeo.btngroupDirective');
 /** @suppress {extraRequire} */
+goog.require('ngeo.PrintUtils');
+/** @suppress {extraRequire} */
+goog.require('ngeo.CreatePrint');
+/** @suppress {extraRequire} */
 goog.require('ngeo.resizemapDirective');
 goog.require('ol.Map');
 goog.require('ol.View');
@@ -84,6 +88,42 @@ gmf.AbstractDesktopController = function(config, $scope, $injector) {
       this.loginActive = false;
     }
   }.bind(this));
+
+
+  var PRINT_SCALES_ = [100, 250, 500, 2500, 5000, 10000, 25000, 50000,
+    100000, 500000];
+  var PRINT_PAPER_SIZE_ = [555, 675];
+
+  var ngeoPrintUtils = $injector.get('ngeoPrintUtils');
+
+  /**
+   * @type {function(ol.render.Event)}
+   */
+  var postcomposeListener = ngeoPrintUtils.createPrintMaskPostcompose(
+      /**
+       * @return {ol.Size} Size in dots of the map to print.
+       */
+      function() {
+        return PRINT_PAPER_SIZE_;
+      },
+      /**
+       * @param {olx.FrameState} frameState Frame state.
+       * @return {number} Scale of the map to print.
+       */
+      function(frameState) {
+        var mapSize = frameState.size;
+        var mapResolution = frameState.viewState.resolution;
+        // we test mapSize and mapResolution just to please the compiler
+        return mapSize !== undefined && mapResolution !== undefined ?
+            ngeoPrintUtils.getOptimalScale(mapSize, mapResolution,
+                PRINT_PAPER_SIZE_, PRINT_SCALES_) :
+            PRINT_SCALES_[0];
+      });
+
+  /**
+   * Draw the print window in a map postcompose listener.
+   */
+  this.map.on('postcompose', postcomposeListener);
 
 };
 goog.inherits(gmf.AbstractDesktopController, gmf.AbstractController);
