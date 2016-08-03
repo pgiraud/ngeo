e5e36aa956a7070e88cf4065413feca414994785
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2016-08-03 13:47:09 +0200

    Update GitHub pages

---
+++ examples_https/examples/https.js
@@ -0,0 +1,10 @@
+/*
+ * Redirect to https
+ */
+if (window.location.protocol == 'http:' &&
+    window.location.port != '3000' &&
+    window.location.host != 'localhost') {
+  var restOfUrl = window.location.href.substr(5);
+  /** @type {Location} */
+  window.location = 'https:' + restOfUrl;
+}
